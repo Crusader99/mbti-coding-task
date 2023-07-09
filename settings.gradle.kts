827d246d1981f9630000a6d2a15ee754bb1d0b97
Simon Forschner <26634807+Crusader99@users.noreply.github.com>
2023-07-09 16:30:57 +0200

    build: initialize Gradle structure (#6)

---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+/*
+ * The settings file is used to specify which projects to include in your build.
+ * For more detailed information, please refer to https://docs.gradle.org/current/userguide/organizing_gradle_projects.html in the Gradle documentation.
+ */
+
+rootProject.name = "mbti-coding-task"
