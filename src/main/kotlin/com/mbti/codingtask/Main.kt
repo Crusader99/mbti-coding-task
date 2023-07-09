3622e6a3078c571394b7a6037fe15f2013762d27
Simon Forschner <26634807+Crusader99@users.noreply.github.com>
2023-07-09 19:55:57 +0200

    feat: add console application for testing (close #8)

--- src/main/kotlin/com/mbti/codingtask/Main.kt
+++ src/main/kotlin/com/mbti/codingtask/Main.kt
@@ -1,10 +1,51 @@
 package com.mbti.codingtask
 
+import kotlin.system.exitProcess
+
 
 /**
- * Entry point of this Kotlin application.
+ * Entry point of a simple console application for testing interval merging.
  */
 fun main() {
-    val intervals = listOf(25..30, 2..19, 14..23, 4..8)
-    println(merge(intervals))
+    println("Welcome to the Interval Merge Application!")
+    println("Please enter intervals that should be merged in the format '[start,end]', each separated a space:")
+
+    // Parse console input with error handling
+    val inputIntervals = try {
+        readln().parseIntervalsToList()
+    } catch (ex: Exception) {
+        System.err.println("Invalid input format!")
+        ex.printStackTrace()
+        exitProcess(1)
+    }
+
+    val mergedIntervals = merge(inputIntervals)
+    println("Input: ${inputIntervals.formatToString()}")
+    println("Output: ${mergedIntervals.formatToString()}")
+}
+
+/**
+ * Extension function to convert a [String] with intervals in format '[start,end]',
+ * each separated a space into a [List] of [Interval] objects.
+ *
+ * @return [Interval] objects parsed from the given string.
+ */
+private fun String.parseIntervalsToList(): List<Interval> {
+    return trim().split(" ").filter { it.isNotBlank() }.map { intervalText ->
+        val (start, end) = intervalText.removeSurrounding("[", "]").split(",", ";").map { it.toInt() }
+        Interval(start, end)
+    }
+}
+
+/**
+ * Extension function to convert a list of [Interval] objects to a human-readable string.
+ *
+ * @return String in a human-readable format. Example output: [2,23] [25,30].
+ */
+private fun List<Interval>.formatToString(): String {
+    return joinToString(" ") { interval ->
+        val start = interval.first
+        val end = interval.last
+        "[$start,$end]"
+    }
 }
