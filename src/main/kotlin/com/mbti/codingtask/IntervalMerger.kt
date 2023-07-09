caba53334754f886e0a39cba5df793b87ae93e05
Simon Forschner <26634807+Crusader99@users.noreply.github.com>
2023-07-09 18:22:57 +0200

    feat: initial implementation of merge algorithm (#4)

--- src/main/kotlin/com/mbti/codingtask/IntervalMerger.kt
+++ src/main/kotlin/com/mbti/codingtask/IntervalMerger.kt
@@ -1,5 +1,8 @@
 package com.mbti.codingtask
 
+import java.util.*
+
+
 /**
  * Reuse existing [IntRange] class from standard library as interval.
  */
@@ -12,5 +15,28 @@ typealias Interval = IntRange
  * @return List of [Interval] objects that do not intersect.
  */
 fun merge(intervals: List<Interval>): List<Interval> {
-    TODO("Currently not implemented")
+    // Sort intervals based on their start value
+    val sortedIntervals = intervals.ifEmpty { return emptyList() }.sortedBy { it.first }
+
+    // Initialize result list with the first interval
+    val mergedIntervals = LinkedList<IntRange>().apply { this += sortedIntervals.first() }
+
+    // Iterate over the remaining intervals
+    for (currentInterval in sortedIntervals.asSequence().drop(1)) {
+        val lastMergedInterval = mergedIntervals.last()
+
+        // Check if the current interval overlaps with the last merged interval
+        if (currentInterval.first <= lastMergedInterval.last) {
+            // Update the end time of the last merged interval if needed
+            if (currentInterval.last > lastMergedInterval.last) {
+                // Create new interval insteadof modifying interval to prevent possible side effects on caller side
+                mergedIntervals[mergedIntervals.lastIndex] = lastMergedInterval.first..currentInterval.last
+            }
+        } else {
+            // Add because interval does not overlap
+            mergedIntervals += currentInterval
+        }
+    }
+
+    return mergedIntervals
 }
